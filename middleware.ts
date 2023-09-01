4b764e48e30f9acfc0322cfbf38f86c7bec3865e
Arian-Collaso <arian.collaso.rodriguez@gmail.com>
2023-09-01 23:42:42 +0200

    get back and doing well

--- middleware.ts
+++ middleware.ts
@@ -70,52 +70,54 @@ const getLocalePartsFrom = ({ pathname, locale }: LocaleSource) => {
 
 // ------------------------------------------- Middleware
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
+  const pathname = request.nextUrl.pathname;
 
-    // Saltamos cualquier ruta que este dentro de assets,
-    if (pathname.match(/\/(assets|public|_next|img)\//)) {
+  if (pathname.match(/\/(assets|public|_next|img)\//)) {
     return NextResponse.next();
   }
-    const defaultLocaleParts = getLocalePartsFrom({ locale: defaultLocale });
 
-    const currentPathnameParts = getLocalePartsFrom({ pathname });
 
-    console.log(currentPathnameParts, " este es el currentPathnameParts"
-    , defaultLocaleParts, " este es el defaultLocaleParts")
-    
-  
-    const pathnameIsMissingValidLocale = locales.every((locale) => {
-      
-        const localeParts = getLocalePartsFrom({ locale });
-        return !pathname.startsWith(`/${localeParts.lang}/${localeParts.country}`);
-      })
-    
-      if (pathnameIsMissingValidLocale) {
-        const matchedLocale = findBestMatchingLocale(
-          request.headers.get("Accept-Language") || defaultLocale
-        );
-    
-        console.log(matchedLocale , " este es el locale del navegador")
-        if (matchedLocale !== defaultLocale) {
-          const matchedLocaleParts = getLocalePartsFrom({ locale: matchedLocale });
-          return NextResponse.redirect(
-            new URL(
-              `/${matchedLocaleParts.lang}/${matchedLocaleParts.country}${pathname}`,
-              request.url
-            )
-          );
-        } else {
-          return NextResponse.rewrite(
-            new URL(
-              `/${defaultLocaleParts.lang}/${defaultLocaleParts.country}${pathname}`,
-              request.url
-            )
-          );
+  const defaultLocaleParts = getLocalePartsFrom({ locale: defaultLocale });
+  const currentPathnameParts = getLocalePartsFrom({ pathname });
 
-        }
-      }
+  // Check if the default locale is in the pathname
+  if (
+    currentPathnameParts.lang === defaultLocaleParts.lang &&
+    currentPathnameParts.country === defaultLocaleParts.country
+  ) {
+    // we want to REMOVE the default locale from the pathname,
+    // and later use a rewrite so that Next will still match
+    // the correct code file as if there was a locale in the pathname
+    return NextResponse.redirect(
+      new URL(
+        pathname.replace(
+          `/${defaultLocaleParts.lang}/${defaultLocaleParts.country}`,
+          pathname.startsWith("/") ? "/" : ""
+        ),
+        request.url
+      )
+    );
+
     
+
+
   }
+  
+  const pathnameIsMissingValidLocale = locales.every((locale) => {
+    const localeParts = getLocalePartsFrom({ locale });
+    return !pathname.startsWith(`/${localeParts.lang}/${localeParts.country}`);
+  });
+
+  if (pathnameIsMissingValidLocale) {
+    // rewrite it so next.js will render `/` as if it was `/es/us` 
+    return NextResponse.rewrite(
+      new URL(
+        `/${defaultLocaleParts.lang}/${defaultLocaleParts.country}${pathname}`,
+        request.url
+      )
+    );
+  }
+}
 
 
 
